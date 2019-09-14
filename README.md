f01bab706dac07e3d53a81a4097f5a0d1bfa4c81
Aquafortis <support@320up.com>
2019-09-14 22:43:43 +1000

    Tagging version 1.7

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 ## Chrome Extension
 ### Making the Lincoln website even better
 
-This Extension will make the Lincoln website easier to read.
+This Extension will make the Lincoln website easier to use.
 
 - Borders around product information in search view
 - Border around product image on product page
+- Shortcut to sitemap page and categories
 - Sitemap links open in new tab
 - Option to hide chat widget
 - More added as required ...
+
+Search for products, enter orders, view latest releases, and more from a popup window even if you are on another website.
 
 Install from: local file
--- popup/popup.js
+++ popup/popup.js
@@ -2,14 +2,21 @@
  * Copyright (c) 2019 Aquafortis
  * Licensed under MPL-2.0 (https://github.com/Aquafortis/lincoln-custom)
  */
+const chat = document.getElementById("widget");
+
 function saveOptions(e) {
     e.preventDefault();
+
+    if (chat.value == "Show") {
+        chat.value = "Hide";
+        chat.setAttribute("src", "../icons/chat-off-32.png");
+    } else {
+        chat.value = "Show";
+        chat.setAttribute("src", "../icons/chat-on-32.png");
+    }
     chrome.storage.sync.set({
-        widget: document.querySelector("#widget").value
+        widget: chat.value
     });
-    setTimeout(function() {
-        window.close();
-    }, 100);
 }
 
 function restoreOptions() {
@@ -17,8 +24,17 @@ function restoreOptions() {
         "widget"
     ];
     chrome.storage.sync.get(keys, function(res) {
-        document.querySelector("#widget").value = res.widget || "Show";
+        chat.value = res.widget || "Show";
     });
+}
+
+function openHomepage() {
+    chrome.tabs.update({
+        url: "https://www.lincolnsentry.com.au/"
+    });
+    setTimeout(function() {
+        window.close();
+    }, 100);
 }
 
 function openSitemap() {
@@ -30,6 +46,14 @@ function openSitemap() {
     }, 100);
 }
 
+function addFrame() {
+    const frame = document.getElementById("latest");
+    frame.setAttribute("src",
+        "https://www.lincolnsentry.com.au/general-information/latest_releases");
+}
+
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.getElementById("home").addEventListener("click", openHomepage);
 document.getElementById("sitemap").addEventListener("click", openSitemap);
+document.getElementById("widget").addEventListener("click", saveOptions);
+window.addEventListener("load", addFrame);
